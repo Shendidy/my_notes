2c60da649f4023f83e71043c196fb74f43b2c8fd
Sherif Shendidy <sherif@shendidy.com>
2019-02-24 16:08:40 +0000

    Before trying user account with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   resources :notes, :categories
 
-  get 'notes/index'
-  get 'notes/edit'
-  get 'notes/new'
-  get 'notes/show'
-  get 'categories/index'
-  get 'categories/edit'
-  get 'categories/new'
-  get 'categories/show'
-  get 'home/index'
+  # get 'notes/index'
+  # get 'notes/edit'
+  # get 'notes/new'
+  # get 'notes/show'
+  # get 'categories/index'
+  # get 'categories/edit'
+  # get 'categories/new'
+  # get 'categories/show'
+  # get 'home/index'
 
   root to:'home#index'
 end
