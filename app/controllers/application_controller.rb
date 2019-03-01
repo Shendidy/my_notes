35323dfac63792855092b135e1b446b3c01f4954
Sherif Shendidy <sherif@shendidy.com>
2019-03-01 11:04:05 +0000

    fixed conflicts!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # ----------
 
   def after_sign_in_path_for(resource)
-    categories_path
+    notes_path
   end
 
   # To facilitate signing up with a user_name or email
