315cd4ad59ecc4a934dda30869b45885ca75dde2
Sherif Shendidy <sherif@shendidy.com>
2019-03-16 02:47:49 +0000

    fixed initial layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :notes, :categories
 
-  # get 'notes/index'
-  get 'posts/edit'
-  # get 'notes/new'
-  # get 'notes/show'
-  # get 'categories/index'
-  # get 'categories/edit'
-  # get 'categories/new'
-  # get 'categories/show'
-  # get 'home/index'
-
   root to:'home#index'
 end
