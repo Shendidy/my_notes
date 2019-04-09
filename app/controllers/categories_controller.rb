37eb66b6938b7e4ba7105285f041442b0ff42b5e
Shendidy <sherif@shendidy.com>
2019-04-09 02:41:42 +0100

    final commit before my freview

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.where(user_id: current_user.id)
+    @categories = Category.where(user_id: current_user.id).order("name")
   end
 
   def show
